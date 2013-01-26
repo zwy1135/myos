a9b9f68c61260e922a2cb12a7e237b96138793b1
zwy1135 <zwy1135@gmail.com>
2013-01-26 18:44:09 +0800

    完成到6天c
    错误以解决

--- graphic.c
+++ graphic.c
@@ -46,9 +46,12 @@ void set_palette(int start,int end,unsigned char *rgb)
 void boxfill8(unsigned char *vram,int xsize,unsigned char c,int x0,int y0,int x1,int y1)
 {
 	int x,y;
-	for(y=y0;y<y1;y++)
-		for(x=x0;x<x1;x++)
+	for(y=y0;y<=y1;y++)
+	{
+		for(x=x0;x<=x1;x++)
 			vram[y*xsize+x]=c;
+	}
+	
 	return;
 }
 
