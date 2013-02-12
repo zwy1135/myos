fd8a1211b37bb3af4a231247124e95f431c7c259
zwy1135 <zwy1135@gmail.com>
2013-02-12 16:14:09 +0800

    完成第十天a
    整理完成，运行正常

--- bootpack.h
+++ bootpack.h
@@ -136,3 +136,29 @@ void inthandler2c(int *esp);
 #define KEYCMD_SENDTO_MOUSE		0xd4
 #define MOUSECMD_ENABLE			0xf4
 
+//memory.c
+#define MEMMAN_FREES		4090    //大约32kb
+#define MEMMAN_ADDR			0x003c0000
+#define EFLAGS_AC_BIT		0x00040000
+#define CR0_CACHE_DISABLE	0x60000000
+
+struct FREEINFO						//可用信息
+{
+	unsigned int addr,size;
+};
+
+struct MEMMAN						//内存管理
+{
+	int frees,maxfrees,lostsize,losts;
+	struct FREEINFO free[MEMMAN_FREES];
+};
+
+unsigned int memtest(unsigned int start,unsigned int end);
+void memman_init(struct MEMMAN *man);
+unsigned int memman_total(struct MEMMAN *man);
+unsigned int memman_alloc(struct MEMMAN *man,unsigned int size);
+int memman_free(struct MEMMAN *man,unsigned int addr,unsigned int size);
+unsigned int memman_alloc_4k(struct MEMMAN *man,unsigned int size);
+int memman_free_4k(struct MEMMAN *man,unsigned int addr,unsigned int size);
+
+
